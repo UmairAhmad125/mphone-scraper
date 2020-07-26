a3b2cde90954e48dd55c3919ee414a00ff2526cd
umairahmad125 <umairahmad125@gmail.com>
2020-07-27 01:39:14 +0500

    remove lint errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,8 @@
-# frozen_string_literal: true
-
 require 'mechanize'
 require 'byebug'
 require_relative '../lib/raw_page.rb'
 require_relative '../lib/clean_data.rb'
 require_relative '../lib/delete_nil.rb'
-
-def remove_empty_product(arr)
-  clean_counter = 0
-  clean_arr = []
-  while clean_counter < arr.length
-    clean_arr[clean_counter] = arr[clean_counter + 1]
-    clean_counter += 1
-  end
-  clean_arr
-end
 
 def clean_price(array)
   temp_clean_array = array.split('PKR')
@@ -36,12 +24,12 @@ hs_page = home_shopping_obj.raw_page
 i_shopping_obj = RawPage.new
 i_shopping_obj.url = url_i
 i_page = i_shopping_obj.raw_page
-homeShopping_phones = hs_page.search('div.product-box')
-iShopping_phones = i_page.search('div.inner-grid')
-iShopping_phones_details = iShopping_phones.css('h2.product-name a').text.to_s
-iShopping_phones_price = iShopping_phones.css('span.price').text.to_s
-hs_dets =  homeShopping_phones.css('div.product-box a').text.to_s
-dets_ish = iShopping_phones_details.split('Samsung')
+home_shopping_phones = hs_page.search('div.product-box')
+i_shopping_phones = i_page.search('div.inner-grid')
+i_shopping_phones_details = i_shopping_phones.css('h2.product-name a').text.to_s
+i_shopping_phones_price = i_shopping_phones.css('span.price').text.to_s
+hs_dets =  home_shopping_phones.css('div.product-box a').text.to_s
+dets_ish = i_shopping_phones_details.split('Samsung')
 remove_empty = DeleteNil.new
 remove_empty.array = dets_ish
 pre_clean_dets_ish = remove_empty.remove_empty_product
@@ -49,17 +37,19 @@ clean_dets_ish = []
 (pre_clean_dets_ish.length - 1).times do |i|
   clean_dets_ish[i] = 'Samsung' + pre_clean_dets_ish[i]
 end
-ish_clean_price = clean_price(iShopping_phones_price)
+ish_clean_price = clean_price(i_shopping_phones_price)
 
 i_shopping_clean = CleanData.new
 i_shopping_clean.array = clean_dets_ish
 i_shopping_clean.price = ish_clean_price
-i_shopping_products = i_shopping_clean.get_details
+i_shopping_products = i_shopping_clean.details
 dets_array = hs_dets.split("\n\n\n")
 remove_empty_arr = DeleteNil.new
 remove_empty_arr.array = dets_array
 clean_dets = remove_empty_arr.remove_empty_product
 hs_phones_details = CleanData.new
 hs_phones_details.array = clean_dets
-h_shopping_products = hs_phones_details.get_details_hs
+h_shopping_products = hs_phones_details.details_hs
 puts h_shopping_products
+puts
+puts i_shopping_products
--- lib/clean_data.rb
+++ lib/clean_data.rb
@@ -1,14 +1,10 @@
-# frozen_string_literal: true
-
-require 'byebug'
 class CleanData
   attr_writer :array
   attr_writer :price
-  def get_details
+  def details
     array = @array
     price = @price
     i = 0
-    separated = {}
     model = []
     specs = []
     phone_details = []
@@ -20,8 +16,8 @@ class CleanData
       chk_fourth = spec_array[3]
       tmp_model = []
       tmp_spec = []
+      j = 0
       if chk_fourth.length > 2 && chk_fourth[3].class == String
-        j = 0
         while j < 3
           tmp_model[j] = spec_array[j]
           j += 1
@@ -32,7 +28,6 @@ class CleanData
         end
 
       else
-        j = 0
         while j < 4
           tmp_model[j] = spec_array[j]
           j += 1
@@ -55,38 +50,36 @@ class CleanData
     end
     phone_details
   end
-  def get_details_hs
+
+  def details_hs
     clean_dets = @array
     clean_counter = 0
     actual_details = []
-    actual_price = []
+    price = []
     model = []
     specs = []
     warranty = []
     while clean_counter < clean_dets.length - 1
-        temp_arr = clean_dets[clean_counter].split('Rs')
-        actual_details[clean_counter] = temp_arr[0].strip
-        temp_model = actual_details[clean_counter].split('(')
-        model[clean_counter] = temp_model[0].strip
-        temp_specs = temp_model[1].split(')')
-        specs[clean_counter] = temp_specs[0].strip
-        warranty[clean_counter] = temp_specs[1].strip
-        actual_price[clean_counter] = temp_arr[1].gsub(/[\s,]/, '').to_i
-        clean_counter += 1
+      temp_arr = clean_dets[clean_counter].split('Rs')
+      actual_details[clean_counter] = temp_arr[0].strip
+      temp_model = actual_details[clean_counter].split('(')
+      model[clean_counter] = temp_model[0].strip
+      temp_specs = temp_model[1].split(')')
+      specs[clean_counter] = temp_specs[0].strip
+      warranty[clean_counter] = temp_specs[1].strip
+      price[clean_counter] = temp_arr[1].gsub(/[\s,]/, '').to_i
+      clean_counter += 1
     end
     hs_phones = []
-    i = 0
-    (clean_dets.length-1).times do |i|
-    phone_info = {
+    (clean_dets.length - 1).times do |i|
+      phone_info = {
         model: model[i],
         specs: specs[i],
         warranty: warranty[i],
-        price: actual_price[i]
-    }
-    i += 1
-    hs_phones << phone_info
+        price: price[i]
+      }
+      hs_phones << phone_info
     end
     hs_phones
   end
-  
 end
