568b799a4d721fabccdb29c254a894e1192a7fe6
umairahmad125 <umairahmad125@gmail.com>
2020-07-27 18:06:28 +0500

    refactored code and add test cases

--- bin/main.rb
+++ bin/main.rb
@@ -17,52 +17,88 @@ def clean_price(array)
   clean_array
 end
 
-url = 'https://homeshopping.pk/categories/Samsung-Mobile-Prices-In-Pakistan/'
-url_i = 'https://www.ishopping.pk/electronics/mobile-phones-tablet-pc/mobile-phones-prices-in-pakistan/samsung.html'
-home_shopping_obj = RawPage.new
-home_shopping_obj.url = url
-hs_page = home_shopping_obj.raw_page
-i_shopping_obj = RawPage.new
-i_shopping_obj.url = url_i
-i_page = i_shopping_obj.raw_page
-home_shopping_phones = hs_page.search('div.product-box')
-i_shopping_phones = i_page.search('div.inner-grid')
-i_shopping_phones_details = i_shopping_phones.css('h2.product-name a').text.to_s
-i_shopping_phones_price = i_shopping_phones.css('span.price').text.to_s
-hs_dets = home_shopping_phones.css('div.product-box a').text.to_s
-dets_ish = i_shopping_phones_details.split('Samsung')
-remove_empty = DeleteNil.new
-remove_empty.array = dets_ish
-pre_clean_dets_ish = remove_empty.remove_empty_product
-clean_dets_ish = []
-(pre_clean_dets_ish.length - 1).times do |i|
-  clean_dets_ish[i] = 'Samsung' + pre_clean_dets_ish[i]
+def hs_url
+  url = 'https://homeshopping.pk/categories/Samsung-Mobile-Prices-In-Pakistan/'
+  home_shopping_obj = RawPage.new
+  home_shopping_obj.url = url
+  hs_page = home_shopping_obj.raw_page
+  hs_page
 end
-ish_clean_price = clean_price(i_shopping_phones_price)
 
-i_shopping_clean = CleanData.new
-i_shopping_clean.array = clean_dets_ish
-i_shopping_clean.price = ish_clean_price
-i_shopping_products = i_shopping_clean.details
-dets_array = hs_dets.split("\n\n\n")
-remove_empty_arr = DeleteNil.new
-remove_empty_arr.array = dets_array
-clean_dets = remove_empty_arr.remove_empty_product
-hs_phones_details = CleanData.new
-hs_phones_details.array = clean_dets
-h_shopping_products = hs_phones_details.details_hs
-write_hs = CsvMaker.new
-write_hs.array = h_shopping_products
+def is_url
+  url_i = 'https://www.ishopping.pk/electronics/mobile-phones-tablet-pc/mobile-phones-prices-in-pakistan/samsung.html'
+  i_shopping_obj = RawPage.new
+  i_shopping_obj.url = url_i
+  i_page = i_shopping_obj.raw_page
+  i_page
+end
+
+def hs_search
+  home_shopping_phones = hs_url.search('div.product-box')
+  hs_dets = home_shopping_phones.css('div.product-box a').text.to_s
+  dets_array = hs_dets.split("\n\n\n")
+  dets_array
+end
+
+def remove_empty_array(search)
+  remove_empty_arr = DeleteNil.new
+  remove_empty_arr.array = search
+  clean_dets = remove_empty_arr.remove_empty_product
+  clean_dets
+end
+
+def hs_refined_data
+  hs_phones_details = CleanData.new
+  hs_phones_details.array = remove_empty_array(hs_search)
+  h_shopping_products = hs_phones_details.details_hs
+  h_shopping_products
+end
+
+def is_search
+  i_shopping_phones = is_url.search('div.inner-grid')
+  i_shopping_phones_details = i_shopping_phones.css('h2.product-name a').text.to_s
+  dets_ish = i_shopping_phones_details.split('Samsung')
+  dets_ish
+end
+
+def is_pre_refined_data
+  pre_clean_dets_ish = remove_empty_array(is_search)
+  clean_dets_ish = []
+  (pre_clean_dets_ish.length - 1).times do |i|
+    clean_dets_ish[i] = 'Samsung' + pre_clean_dets_ish[i]
+  end
+  clean_dets_ish
+end
+
+def is_price
+  i_shopping_phones = is_url.search('div.inner-grid')
+  i_shopping_phones_price = i_shopping_phones.css('span.price').text.to_s
+  ish_clean_price = clean_price(i_shopping_phones_price)
+  ish_clean_price
+end
+
+def is_refined_data
+  i_shopping_clean = CleanData.new
+  i_shopping_clean.array = is_pre_refined_data
+  i_shopping_clean.price = is_price
+  i_shopping_products = i_shopping_clean.details
+  i_shopping_products
+end
+
+write_hs = CsvMaker.new # write hs_phones in file
+write_hs.array = hs_refined_data
 write_hs.write_file_hs
-write_is = CsvMaker.new
-write_is.array = i_shopping_products
+write_is = CsvMaker.new # write is_phones in file
+write_is.array = is_refined_data
 write_is.write_file_is
-compare = ComparePrices.new
-compare.is_array = i_shopping_products
-compare.hs_array = h_shopping_products
+
+compare = ComparePrices.new # compare similar phone models
+compare.is_array = is_refined_data
+compare.hs_array = hs_refined_data
 comparison_array = compare.compare
-puts h_shopping_products
+
+puts hs_refined_data
 puts
-puts i_shopping_products
+puts is_refined_data
 puts
 puts comparison_array
---
+++ spec/test_cases.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+require_relative '../lib/clean_data'
+require_relative '../lib/compare_prices'
+require_relative '../lib/delete_nil'
+require_relative '../lib/make_csv'
+require_relative '../lib/raw_page'
+require 'mechanize'
+
+describe 'CleanData' do
+  let(:hs_obj) { CleanData.new }
+  let(:is_obj) { CleanData.new }
+  let(:hs_array) { ['Samsung Galaxy Note 10 Plus Dual Sim (4G, 12GB RAM, 256 ROM,Aura Black) With Official Warranty  Rs 182,999', 'Samsung Galaxy A31 (4G, 4GB, 128GB, Blue) With Official Warranty Rs 40,199'] }
+  let(:is_array) { ['Samsung Galaxy A10s 32GB Dual Sim Red - Official Warranty', 'Samsung Galaxy A10s 32GB Dual Sim Black - Official Warranty'] }
+  let(:is_price) { [1_312_312, 3_123_213] }
+  describe 'details_hs' do
+    it 'should creturn array of hash' do
+      hs_obj.array = hs_array
+      hashed_array = hs_obj.details_hs
+      expect(hashed_array[0].class).to eq(Hash)
+    end
+  end
+
+  describe 'details' do
+    it 'should creturn array of hash' do
+      is_obj.array = is_array
+      is_obj.price = is_price
+      hashed_array = is_obj.details
+      expect(hashed_array[0].class).to eq(Hash)
+    end
+  end
+end
+
+describe 'ComparePrices' do
+  let(:comp_obj) { ComparePrices.new }
+  describe 'details' do
+    let(:is_array) { [{ model: 'Samsung Galaxy A10s', specs: '32GB Dual Sim Red', warranty: 'Official Warranty', price: 18_500 }, { model: 'Samsung Galaxy A10s', specs: '32GB Dual Sim Black', warranty: 'Official Warranty', price: 18_500 }] }
+    let(:hs_array) { [{ model: 'Samsung Galaxy Note 10 Plus Dual Sim', specs: '4G, 12GB RAM, 256 ROM,Aura Black', warranty: 'With Official Warranty', price: 182_999 }, { model: 'Samsung Galaxy A31', specs: '4G, 4GB, 128GB, Blue', warranty: 'With Official Warranty', price: 40_199 }] }
+    it 'should return array of 0 length if phones are not of same model' do
+      comp_obj.hs_array = hs_array
+      comp_obj.is_array = is_array
+      hashed_array = comp_obj.compare
+      expect(hashed_array.length).to be(0)
+    end
+    it 'should return array of containing phone details if phone models match' do
+      comp_obj.hs_array = is_array
+      comp_obj.is_array = is_array
+      hashed_array = comp_obj.compare
+      expect(hashed_array.length).to be(2)
+    end
+    it 'should return array of containing phone details if phone models match' do
+      comp_obj.hs_array = is_array
+      comp_obj.is_array = is_array
+      hashed_array = comp_obj.compare
+      expect(hashed_array[0].class).to be(Hash)
+    end
+  end
+end
+
+describe 'DeleteNil' do
+  let(:array) { [1, 2, 3] }
+  let(:obj) { DeleteNil.new }
+  describe 'remove_empty_product' do
+    it 'should remove the first element from array and replace last with nil' do
+      obj.array = array
+      aray = obj.remove_empty_product
+      expect(aray).to eq([2, 3, nil])
+    end
+  end
+end
+
+describe 'CsvMaker' do
+  let(:is_array) { [{ model: 'Samsung Galaxy A10s', specs: '32GB Dual Sim Red', warranty: 'Official Warranty', price: 18_500 }, { model: 'Samsung Galaxy A10s', specs: '32GB Dual Sim Black', warranty: 'Official Warranty', price: 18_500 }] }
+  let(:hs_array) { [{ model: 'Samsung Galaxy Note 10 Plus Dual Sim', specs: '4G, 12GB RAM, 256 ROM,Aura Black', warranty: 'With Official Warranty', price: 182_999 }, { model: 'Samsung Galaxy A31', specs: '4G, 4GB, 128GB, Blue', warranty: 'With Official Warranty', price: 40_199 }] }
+  let(:obj) { CsvMaker.new }
+  describe 'write_file_hs' do
+    it 'should write given data in hs_phones.csv file' do
+      obj.array = hs_array
+      expect(File.file?('hs_phones.csv')).to be(true)
+    end
+  end
+  describe 'write_file_is' do
+    it 'should write given data in is_phones.csv file' do
+      obj.array = is_array
+      expect(File.file?('is_phones.csv')).to be(true)
+    end
+  end
+end
+
+describe 'RawPage' do
+  describe 'raw_page' do
+    let(:url) { 'https://vimeo.com/ondemand/jorgemasvidal' }
+    agent = Mechanize.new
+    let(:raw_obj) { RawPage.new }
+    it 'should return raw page' do
+      test_page = agent.get(url)
+      raw_obj.url = url
+      page = raw_obj.raw_page
+      expect(page.title).to eq(test_page.title)
+    end
+  end
+end
